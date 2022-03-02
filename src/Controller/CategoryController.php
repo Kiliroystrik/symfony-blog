40e5da540940c1b868e832ef6f16054f4f64be8c
Kiliroy <borges.mathieu@gmail.com>
2022-03-02 18:27:51 +0100

    added route to display all posts by chosen category added route to create category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Post;
+use App\Form\CategoryFormType;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
 {
@@ -19,6 +24,44 @@ class CategoryController extends AbstractController
             'categories' => $allCategory,
         ]);
     }
+
+    #[Route('/category/{id}', name: 'category-posts')]
+    public function allPosts(int $id, CategoryRepository $categoryRepository, Category $category): Response
+    {
+        $categoryRepository = $categoryRepository->findAll($id);
+
+
+
+        return $this->render('category/postbycatego.html.twig', [
+            'postByCategory' => $categoryRepository,
+            'id' => $category->getId(),
+        ]);
+    }
+
+
+    #[Route('/addcategory', name: 'add-category')]
+    public function createCategory(Request $request, EntityManagerInterface $entityManagerInterface): Response
+    {
+
+        $category = new Category();
+
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManagerInterface->persist($category);
+            $entityManagerInterface->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->renderForm('category/add.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+
+
 
     // #[Route('/category', name: 'app_category')]
     // public function index(CategoryRepository $categoryRepository): Response
